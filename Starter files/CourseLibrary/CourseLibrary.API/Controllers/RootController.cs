229b8aa9c03e27a1abbb10c506e0c7cd5d14b9e9
Florian <florian@zocalo-ict.nl>
2020-11-20 22:59:10 +0100

    Add RootController that gives root hateoas links

---
+++ Starter files/CourseLibrary/CourseLibrary.API/Controllers/RootController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using CourseLibrary.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = "GetRoot")]
+        public IActionResult GetRoot()
+        {
+            // create links for root
+            var links = new List<LinkDto>
+            {
+                new LinkDto(Url.Link("GetRoot", new { }),
+                    "self",
+                    "GET"),
+                new LinkDto(Url.Link("GetAuthors", new { }),
+                    "authors",
+                    "GET"),
+                new LinkDto(Url.Link("CreateAuthor", new { }),
+                    "create_author",
+                    "POST")
+            };
+
+            return Ok(links);
+        }
+    }
+}
