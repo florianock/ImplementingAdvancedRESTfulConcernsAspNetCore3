fe35f60309c3064b6a3da447b6e522979f021474
Florian <florian@zocalo-ict.nl>
2020-12-08 15:04:09 +0100

    Control shape of return data using Accept headers

--- Starter files/CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
+++ Starter files/CourseLibrary/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -19,6 +19,12 @@ namespace CourseLibrary.API.Controllers
 {
     [ApiController]
     [Route("api/authors")]
+    [Produces("application/json",
+        "application/vnd.marvin.hateoas+json",
+        "application/vnd.marvin.author.full+json",
+        "application/vnd.marvin.author.full.hateoas+json",
+        "application/vnd.marvin.author.friendly+json",
+        "application/vnd.marvin.author.friendly.hateoas+json")]
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
@@ -125,20 +131,46 @@ namespace CourseLibrary.API.Controllers
                 return NotFound();
             }
 
-            if (parsedMediaType.MediaType == "application/vnd.marvin.hateoas+json")
+            var includeLinks =
+                parsedMediaType.SubTypeWithoutSuffix.EndsWith("hateoas", 
+                    StringComparison.InvariantCultureIgnoreCase);
+
+            IEnumerable<LinkDto> links = new List<LinkDto>();
+
+            if (includeLinks)
             {
-                var links = CreateLinksForAuthor(authorId, fields);
-
-                var linkedResourceToReturn =
-                    _mapper.Map<AuthorDto>(authorFromRepo).ShapeData(fields)
-                        as IDictionary<string, object>;
-
-                linkedResourceToReturn.Add("links", links);
-
-                return Ok(linkedResourceToReturn);
+                links = CreateLinksForAuthor(authorId, fields);
             }
 
-            return Ok(_mapper.Map<AuthorDto>(authorFromRepo).ShapeData(fields));
+            var primaryMediaType = includeLinks
+                ? parsedMediaType.SubTypeWithoutSuffix
+                    .Substring(0, parsedMediaType.SubTypeWithoutSuffix.Length - 8) // cut off '.hateoas' part
+                : parsedMediaType.SubTypeWithoutSuffix;
+            
+            // full author
+            if (primaryMediaType == "vnd.marvin.author.full")
+            {
+                var fullResourceToReturn = _mapper.Map<AuthorFullDto>(authorFromRepo)
+                    .ShapeData(fields) as IDictionary<string, object>;
+
+                if (includeLinks)
+                {
+                    fullResourceToReturn.Add("links", links);
+                }
+
+                return Ok(fullResourceToReturn);
+            }
+            
+            // friendly author
+            var friendlyResourceToReturn = _mapper.Map<AuthorDto>(authorFromRepo)
+                .ShapeData(fields) as IDictionary<string, object>;
+
+            if (includeLinks)
+            {
+                friendlyResourceToReturn.Add("links", links);
+            }
+
+            return Ok(friendlyResourceToReturn);
         }
 
         [HttpPost(Name = "CreateAuthor")]
